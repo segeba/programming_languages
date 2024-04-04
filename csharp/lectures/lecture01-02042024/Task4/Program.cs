2ba7ef352f5a7dc52c8770aade21e7c85bc440bd
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-05 00:09:32 +0300

    add lecture01/Task4

---
+++ csharp/lectures/lecture01-02042024/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 38;
+int b = 24;
+int c = 13;
+int d = 8;
+int e = 19;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
