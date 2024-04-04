85c39b23604bc70d5b0387d555f8dce09e59f97c
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-04 23:04:28 +0300

    add variables lecture01/task1

--- csharp/lectures/lecture01-02042024/Task1/Program.cs
+++ csharp/lectures/lecture01-02042024/Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+int value =100;
+double d = 12.33;
+char c = '?';
+bool b = true;
+
+Console.WriteLine(value);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
