4ca130ea6533c12e04e9b464aeb5f44984eff44d
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-05 00:41:33 +0300

    add seminar01-task2

--- csharp/seminars/seminar01-04042024/Program.cs
+++ csharp/seminars/seminar01-04042024/Program.cs
@@ -1,3 +1,20 @@
-﻿Console.WriteLine("Input Number: ");
-int a = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"Quad of {a} -> {a * a}");
+﻿// Console.WriteLine("Input Number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Quad of {a} -> {a * a}");
+
+// Напишите программу, которая на вход принимает два целых числа и проверяет,
+// является ли первое числоквадратом второго.
+
+Console.WriteLine("Input Number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine()); //25
+Console.WriteLine("Input Number 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine()); //5
+
+if(num1 == num2 * num2)
+{
+    System.Console.WriteLine($"{num1} is quad of {num2}");
+}
+else
+{
+    System.Console.WriteLine($"{num1} is not quad of {num2}");
+}
